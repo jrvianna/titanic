0916e79f5763e52ecc590ed3876e9d2fc0a4649c
RNoob <jaymearvianna@gmail.com>
2017-03-07 19:10:49 -0400

    glm with fare

--- hi.R
+++ hi.R
@@ -1,3 +1,8 @@
-print("Hi")
-
 library(dplyr)
+library(tidyr)
+library(ggplot2)
+
+mod <- glm(Survived ~ Fare, data = train, family = binomial)
+summary(mod)
+
+ggplot(train, aes(x = Fare, color = as.factor(Survived))) + geom_density()
